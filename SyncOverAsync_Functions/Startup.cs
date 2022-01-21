b84b24c0968dd491d5035b77cecd83d557c8cb9d
Adi Thakker <Aditya.Thakker@gmail.com>
2022-01-21 16:15:15 -0500

    Add project files.

---
+++ SyncOverAsync_Functions/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+namespace SyncOverAsync_Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();    
+        }
+    }
+}
