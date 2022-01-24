ee564d3f7b64802f553d582e28a9749c2a737ee1
Adi Thakker <Aditya.Thakker@gmail.com>
2022-01-24 15:14:24 -0500

    updated globalusings

--- SyncOverAsync_Functions/globalusings.cs
+++ SyncOverAsync_Functions/globalusings.cs
@@ -3,5 +3,4 @@ global using Microsoft.Azure.WebJobs;
 global using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 global using Microsoft.Azure.WebJobs.Extensions.Http;
 global using Microsoft.Extensions.Logging;
-global using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection;
